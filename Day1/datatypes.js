cab0fb843716d6003cfd6b63d2c4e756334fd9db
Janani <23it021@kpriet.ac.in>
2026-03-21 17:29:46 +0530

    datatypes.js

---
+++ Day1/datatypes.js
@@ -0,0 +1,20 @@
+// DATA TYPES
+
+let name = "Janani"; // string
+let age = 21; // number
+let isStudent = true; // boolean
+let x; // undefined
+let y = null; // null
+
+let numbers = [1, 2, 3]; // array
+
+let person = {
+  name: "Janani",
+  age: 21
+}; // object
+
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isStudent); //boolean
+console.log(typeof x); //undefined
+console.log(typeof y); //object
