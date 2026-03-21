ca87205b33eeb72aecb8e45331bd78fe889d17cf
Janani <23it021@kpriet.ac.in>
2026-03-21 17:32:09 +0530

    operators.js

---
+++ Day1/operators.js
@@ -0,0 +1,20 @@
+// OPERATORS
+
+let a = 10;
+let b = 5;
+
+// arithmetic
+console.log(a + b); //15
+console.log(a - b); //5
+console.log(a * b); //50
+console.log(a / b); //2
+console.log(a % b); //0
+
+// comparison
+console.log("5" == 5);   // true
+console.log("5" === 5);  // false
+
+// logical
+console.log(true && false); //false
+console.log(true || false); //true
+console.log(!true); //false
